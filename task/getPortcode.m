56f49e12b1e4a55b49b8f2226c362f5fb72380ba
Gabe Nespoli <gabenespoli@gmail.com>
2017-11-21 16:37:32 -0500

    update comments

--- task/getPortcode.m
+++ task/getPortcode.m
@@ -1,11 +1,7 @@
 % getPorcode: function to get a portcode for a stimulus filename
-%
-% USAGE: portcode = getPortcode(fname, stimType) fname: [string] absolute or local path to a file. filename should be a number
+% portcode = getPortcode(fname) 
+% fname: [string] absolute or local path to a file. Filename should be a number
 %           e.g. '1.mp3' or '01.wav'
-% stimType: either 'mir' or 'sync'
-%
-% The portcode comprises either 1 (mir) or 2 (sync) and then the filename number.
-% This means that filenames must be a number and in the range 1-99.
 % If the filename is not a number, the portcode 255 is returned.
 
 function portcode = getPortcode(fname)
