7e530ec8a20c11f95c0e4691cc9bb80869187448
Gabe Nespoli <gabenespoli@gmail.com>
2017-11-28 09:17:04 -0500

    main.m: change fid to logfile_fid

--- task/main.m
+++ task/main.m
@@ -34,37 +34,7 @@ try
         disp('inp/outp installation failed!')
     end
 
-    idStr = [id,'_',stimType,'_',trigType];
-    logfile       = fullfile(logFolder, [idStr,'.txt']);
-    trialListFile = fullfile(logFolder, [idStr,'_trialList','.txt']);
-
-    % deal with the case where a logfile already exists
-    continuePreviousLogfile = checkIfLogfileExists(logfile, trialListFile, currentTime);
-    if continuePreviousLogfile
-        % get trial list from before as a cell array
-        temp = readtable(trialListFile, 'Delimiter','\n', 'ReadVariableNames',false);
-        trialList = temp{:,1};
-
-        % use logfile to figure out what trial we're on, then append to the file
-        temp = readtable(logfile);
-        if isempty(temp)
-            startTrial = 1;
-        else
-            startTrial = temp.trial(end) + 1;
-        end
-        fprintf('Starting at trial %i\n', startTrial)
-
-        fid = fopen(logfile, 'a');
-
-    else
-        trialList = getTrialList(stimFolder, stimType, trialListFile);
-        nHeaders = length(logfileHeaders);
-        fid = fopen(logfile, 'w');
-        formatSpec = [repmat('%s,',[1,nHeaders-1]),'%s\n'];
-        fprintf(fid, formatSpec, logfileHeaders{:});
-        startTrial = 1;
-
-    end
+    [trialList, startTrial, logfile_fid] = getTrialList(logFolder, logfileHeaders, stimFolder, stimType, trigType, currentTime);
 
     %% loop trials
     nTrials = length(trialList);
@@ -75,7 +45,7 @@ try
         fprintf('Trial %i / %i\n', trial, nTrials)
         [move, pleasure] = playTrial(stimfile, stimType, trigType, ioObj, address);
 
-        logResponse(fid, id, stimType, trigType, trial, stimfile, move, pleasure);
+        logResponse(logfile_fid, id, stimType, trigType, trial, stimfile, move, pleasure);
 
     end
 
@@ -92,7 +62,7 @@ end
 fclose('all');
 end % end function
 
-function logResponse(fid, id, stimType, trigType, trial, fname, move, pleasure)
+function logResponse(logfile_fid, id, stimType, trigType, trial, fname, move, pleasure)
 [pathstr, name, ext] = fileparts(fname);
 name = [name, ext];
 timestamp = datestr(now, 'yyyy-mm-dd HH:MM:SS');
@@ -100,6 +70,6 @@ timestamp = datestr(now, 'yyyy-mm-dd HH:MM:SS');
 data = {id, stimType, trigType, trial, name, move, pleasure, pathstr, timestamp};
 formatSpec = '%s,%s,%s,%i,%s,%i,%i,%s,%s\n';
 
-fprintf(fid, formatSpec, data{:});
+fprintf(logfile_fid, formatSpec, data{:});
 
 end
