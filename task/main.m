1098d715b27c5c6a972d8e3c238a736b270e1e68
Gabe Nespoli <gabenespoli@gmail.com>
2017-11-28 09:54:29 -0500

    move makeSureFoldersExists.m into main.m

--- task/main.m
+++ task/main.m
@@ -50,3 +50,16 @@ end
 
 fclose('all');
 end
+
+function makeSureFoldersExist(varargin)
+% loops through input args, checks if its a dir, creates it if it isn't
+% and notifies the user
+for i = 1:length(varargin)
+    folder = varargin{i};
+    if ~exist(folder, 'dir')
+        fprintf('Creating folder ''%s''... ', folder)
+        mkdir(folder)
+        fprintf('Done.\n')
+    end
+end
+end
