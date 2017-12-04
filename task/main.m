87590c32aa8f48c8b38a59e37905c6a06754a81f
Gabe Nespoli <gabenespoli@gmail.com>
2017-12-04 14:44:07 -0500

    move logresponse to main

--- task/main.m
+++ task/main.m
@@ -63,3 +63,14 @@ for i = 1:length(varargin)
     end
 end
 end
+
+function logResponse(logfile_fid, id, stimType, trigType, trial, fname, ...
+                     move, pleasure)
+[pathstr, name, ext] = fileparts(fname);
+name = [name, ext];
+timestamp = datestr(now, 'yyyy-mm-dd HH:MM:SS');
+data = {id, stimType, trigType, trial, name, ...
+        move, pleasure, pathstr, timestamp};
+formatSpec = '%s,%s,%s,%i,%s,%i,%i,%s,%s\n';
+fprintf(logfile_fid, formatSpec, data{:});
+end
