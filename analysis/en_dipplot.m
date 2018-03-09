94ea2698710eaf4ebab5bc6b5ef197af4a7d49f1
Gabe Nespoli <gabenespoli@gmail.com>
2018-03-09 15:41:27 -0500

    en_dipplot.m: update with en_getFolder

--- analysis/en_dipplot.m
+++ analysis/en_dipplot.m
@@ -1,30 +1,35 @@
 function en_dipplot(EEG, comps)
 
-dipdir = fullfile('~','local','matlab','eeglab','plugins','dipfit2.3');
-mrifile  = fullfile(dipdir, 'standard_BESA', 'avg152t1.mat');
-pop_dipplot(EEG, comps, ...
-            'mri',          mrifile, ...
-            'projlines',    'on', ...
-            'normlen',      'on');
-
+pop_dipplot(EEG, ...
+    comps, ...
+    'mri',          en_getFolder('mrifile'), ...
+    'projlines',    'on', ...
+    'normlen',      'on');
         
-sources = dipplot(EEG.dipfit.model, ...
-            'mri',          mrifile, ...
-            'normlen',      'on', ...
-            'plot',         'off');
-              
-coords = [sources(comps).talcoord];
-coords = reshape(coords, 3, length(coords) / 3)';
-if length(comps) ~= size(coords, 1), error('Something''s wrong.'), end
-region = tal2region(coords, 5);
 
-for i = 1:length(comps)
-    if ~isempty(region.cellType{i})
-        str = strcat(region.cellType{i}{:});
-    else
-        str = '';
-    end
-    fprintf('Comp %i: %s\n', comps(i), str)
-end
+% get dipole info struct
+
+% sources = dipplot(EEG.dipfit.model, ...
+%     'mri',          en_getFolder('mrifile'), ...
+%     'coordformat',  'MNI', ...
+%     'normlen',      'on', ...
+%     'plot',         'off');
+              
+% TODO sources is only as big as the number of comps with good rv
+%   comps is the number of the comp
+%   need to get indices of comps in sources
+% coords = [sources(comps).talcoord];
+% coords = reshape(coords, 3, length(coords) / 3)';
+% if length(comps) ~= size(coords, 1), error('Something''s wrong.'), end
+% region = tal2region(coords, 5);
+
+% for i = 1:length(comps)
+%     if ~isempty(region.cellType{i})
+%         str = strcat(region.cellType{i}{:});
+%     else
+%         str = '';
+%     end
+%     fprintf('Comp %i: %s\n', comps(i), str)
+% end
         
 end
