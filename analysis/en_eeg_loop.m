88ebf4a43748a7f8b2431cf77422750464dd82bf
Gabe Nespoli <gabenespoli@gmail.com>
2018-03-12 06:34:08 -0400

    en_eeg_loop.m: change some var names

--- analysis/en_eeg_loop.m
+++ analysis/en_eeg_loop.m
@@ -1,9 +1,10 @@
 function en_eeg_loop(id)
 
 d = datestr(now, 'yyyy-mm-dd_HH-MM-SS');
-tstart = tic;
 tid = cell(1, length(id));
 
+tic;
+tStart = toc;
 for i = 1:length(id)
 
     % prepare for id id
@@ -11,7 +12,7 @@ for i = 1:length(id)
     fprintf('Participant ID: %i\n', id(i))
     fprintf('Loop started: %s\n', d)
     fprintf('This ID started: %s\n', datestr(now, 'yyyy-mm-dd_HH-MM-SS'));
-    tstart_id = toc;
+    tStart_id = toc;
 
     try
         en_preprocess_eeg(id(i), 'sync', 'eeg');
@@ -23,21 +24,21 @@ for i = 1:length(id)
     end
 
     % save and print the elapsed time for this id
-    tend_id = toc;
-    ttime_id = (tend_id - tstart_id) / 60; % in minutes
-    tid{i} = [tid{i}, num2str(ttime_id), ' minutes'];
+    tEnd_id = toc;
+    tTime_id = (tEnd_id - tStart_id) / 60; % in minutes
+    tid{i} = [tid{i}, num2str(tTime_id), ' minutes'];
     fprintf('%s\n', tid{i})
     diary off
 end
 
 % save summary of elapsed time for all ids
-fname = fullfile(en_getFolder('looplogs'), [d, '.txt']);
+fname = fullfile(en_getFolder('looplogs'), [d, '_summary.txt']);
 fid = fopen(fname, 'w');
 fprintf(fid, 'Loop summary\n');
 for i = 1:length(id)
     fprintf('%i: %s\n', id(i), tid{i});
 end
-fprintf('Total time: %g hours\n', (toc - tstart) / 60 / 60);
+fprintf('Total time: %g hours\n', (toc - tStart) / 60 / 60);
 fclose(fid);
 
 end
