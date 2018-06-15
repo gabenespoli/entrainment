78bdd3d32c4a9f0105372bd8ce7517ceda7f59d7
Gabe Nespoli <gabenespoli@gmail.com>
2018-06-15 12:10:20 -0400

    region2comps.m: allow input of many cubesizes, return distance for each comp

--- analysis/region2comps.m
+++ analysis/region2comps.m
@@ -16,22 +16,29 @@
 %
 %     'rv'        = Only return components with a residual variance below
 %                   this percentage. Enter 0 to return all components
-%                   regardless of residual variance. Default 15.
+%                   regardless of residual variance. Enter a number
+%                   between 0 and 100. Default 0.
 %
-%     'cubesize'  = See tal2region.m. Default 5 (1.1 cm^3).
+%     'cubesize'  = Number or numeric vector specifying cubsizes to pass
+%                   to tal2region.m. Default [0:5]. If a vector, each
+%                   cubsize is searched, and the smallest value is
+%                   returned. This effectively gives the distance of
+%                   each component to the region.
 %
 % Output:
 %     comps = Numeric vector list of component numbers.
 %
-% Written by Gabriel A. Nespoli 2016-04-25. Revised 2016-04-26.
+%     cubesizes = Numeric vector list of smallest cubsize for each comp.
+%
+% Written by Gabriel A. Nespoli 2016-04-25. Revised 2018-06-13.
 
-function comps = region2comps(EEG, region, varargin)
+function [comps, cubesizes] = region2comps(EEG, region, varargin)
 
 if nargin == 0 && nargout == 0, help region2comps, return, end
 
 % defaults
-rv_threshold = 15;
-cubesize = 5;
+rv_threshold = 0;
+cubesize = 0:5;
 
 % user-defined
 for i = 1:2:length(varargin)
@@ -63,6 +70,10 @@ else
     error('Problem with REGION input.')
 end
 
+if ~isnumeric(cubesize) || ~all(ismember(cubesize, 0:5))
+    error('Problem with CUBESIZE input.')
+end
+
 % get coords for all comps
 coords = {EEG.dipfit.model.posxyz}';
 compsGood = find(~cellfun(@isempty, coords)); % non-empty coords
@@ -77,28 +88,50 @@ switch EEG.dipfit.coordformat
         error('Unknown coordinate format specified in EEG.dipfit.coordformat.')
 end
 
+% loop all cubesizes
+for j = 1:length(cubesize)
+    fprintf('Searching with a cubesize of %i...\n', cubesize(j))
 
-% ------------ this needs attention since tal2region was changed ---------
-% get regions for each comp with specified cubesize (tal2region)
-names = tal2region(coords, cubesize);
-names = names.cellType;
+    % ------------ this needs attention since tal2region was changed ---------
+    % get regions for each comp with specified cubesize (tal2region)
+    locs = tal2region(coords, cubesize(j));
+    names = locs.cellType;
 
-% ------------------------------------------------------------------------
+    % ------------------------------------------------------------------------
 
-% find comps localized to specified Brodmann area(s)
-indComps = [];
-for i = 1:length(region)
-    ind = find(cellfun(@(x) ismember(region{i}, x), names));
-    indComps = unique([indComps; ind]);
+    % find comps localized to specified Brodmann area(s)
+    indComps = [];
+    for i = 1:length(region)
+        ind = find(cellfun(@(x) ismember(region{i}, x), names));
+        indComps = unique([indComps; ind]);
+    end
+    comps = compsGood(indComps);
+
+    % restrict to comps below a certain residual variance
+    if rv_threshold ~= 0
+        rv = {EEG.dipfit.model(compsGood).rv}';
+        rv = cat(1, rv{:});
+        rv = rv(indComps);
+        comps = comps(rv < rv_threshold / 100);
+    end
+
+    cubesizes = repmat(cubesize(j), size(comps));
+
+    if j == 1
+        allComps = comps;
+        allCubesizes = cubesizes;
+    else
+        allComps = [allComps; comps]; %#okAGROW
+        allCubesizes = [allCubesizes; cubesizes]; %#okAGROW
+    end
+
 end
-comps = compsGood(indComps);
+comps = unique(allComps);
 
-% restrict to comps below a certain residual variance
-if rv_threshold ~= 0
-    rv = {EEG.dipfit.model(compsGood).rv}';
-    rv = cat(1, rv{:});
-    rv = rv(indComps);
-    comps = comps(rv < rv_threshold / 100);
+% return smallest cubesize for each comp
+cubesizes = nan(size(comps));
+for i = 1:length(comps)
+    cubesizes(i) = min(allCubesizes(allComps == comps(i)));
 end
 
 end
