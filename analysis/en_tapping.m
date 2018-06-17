fdee5ebd0eea05438225ad1770aacd6f9cb09966
Gabe Nespoli <gabenespoli@gmail.com>
2018-06-16 20:27:51 -0400

    en_tapping: add numMarkersPrompt as an arg

--- analysis/en_tapping.m
+++ analysis/en_tapping.m
@@ -1,7 +1,11 @@
-function en_tapping(ids)
+function en_tapping(ids, numMarkersPrompt)
+%   numMarkersPrompt: 0 to cancel, 1 to prompt, 2 to continue, default 0
 if nargin < 1 || isempty(ids)
     d = en_load('diary', 'incl');
     ids = d.id;
+end
+if nargin < 2 || isempty(numMarkersPrompt)
+    numMarkersPrompt = 0;
 end
 
 stims = {'sync', 'mir'};
@@ -11,7 +15,7 @@ for i = 1:length(ids)
     id = ids(i);
     for j = 1:length(stims)
         stim = stims{j};
-        en_preprocess_tapping(id, stim);
+        en_preprocess_tapping(id, stim, [], numMarkersPrompt);
         en_entrainment_tapping(id, stim, [], matchwarn);
     end
 end
