6c7e75f7f31f1368eaf74a16901d57cb916d72a0
Gabe Nespoli <gabenespoli@gmail.com>
2018-07-15 21:44:18 -0400

    mir functions: automatically make markdown pipe tables

--- stats/mir_functions.R
+++ stats/mir_functions.R
@@ -90,3 +90,124 @@ mir.add.mir <- function(df, folder) {
   # df <- merge(df, mir, by="portcode")
   return(df)
 }
+
+# print cortable as a markdown pipe table ----
+cortable.print <- function(df) {
+  tb <- cortable(df)
+  tb.rownames <- rownames(tb)
+  tb.colnames <- colnames(tb)
+  l <- vector("list", dim(tb)[1] + 1)
+
+  # first row is column headers
+  l[[1]] <- paste("|", "|",
+                  paste(gsub("_", " ", tb.colnames), collapse=" | "),
+                  "|")
+
+  # second row is dashes to separate the header row
+  l[[2]] <- paste("|", strrep("-", max(sapply(tb.rownames, nchar))), "|")
+  for (i in tb.colnames) {
+    l[[2]] <- paste(l[[2]], strrep("-", nchar(i)), "|")
+  }
+
+  # loop rows (list items)
+  nrow <- 3
+  for (rowname in tb.rownames) {
+    l[[nrow]] <- paste("|", rowname)
+    for (colname in tb.colnames) {
+      l[[nrow]] <- paste(l[[nrow]], "|", tb[rowname, colname])
+    }
+    l[[nrow]] <- paste(l[[nrow]], "|")
+    nrow <- nrow + 1
+  }
+
+  # display in console
+  for (ln in l) {
+    cat(ln, "\n")
+  }
+}
+
+# print f string from an lm model ----
+summary.print.f <- function(model) {
+  s <- summary(model)
+  df1 <- s$fstatistic["numdf"]
+  df2 <- s$fstatistic["dendf"]
+  fval <- s$fstatistic["value"]
+  pval <- pf(fval, df1, df2, lower.tail=FALSE)
+  pvalstr <- pval.string(pval)
+  cat("*F*(", df1, ",", df2, ") = ", signif(fval, 4),
+      ", ", pvalstr, "\n", sep="")
+}
+
+# print regression table as a markdown pipe table ----
+summary.print.reg.table <- function(model) {
+  s <- summary(model)
+  s.colnames <- c("Estimate", "t value", "Pr(>|t|)")
+  s.collabels <- c("Variable", "β", "*t*", "*p*")
+  s.rownames <- rownames(s$coefficients)[-1] # remove intercept
+  l <- vector("list", length(s.rownames) + 2)
+
+  # first row is column headers, second row is dashes
+  l[[1]] <- paste("|", paste(s.collabels, collapse=" | "), "|")
+  l[[2]] <- paste("|", strrep("-", max(sapply(s.rownames, nchar))),
+                  "|", strrep(" --------- |", 3), sep="")
+
+  # loop variables and add rows
+  nrow <- 3
+  for (name in s.rownames) {
+    l[[nrow]] <- paste("|", gsub("_", " ", name),
+                       "|", signif(s$coefficients[name, "Estimate"], 4),
+                       "|", signif(s$coefficients[name, "t value"], 4),
+                       "|", pval.string(s$coefficients[name, "Pr(>|t|)"],
+                                        pstr=FALSE),
+                       "|")
+    nrow <- nrow + 1
+  }
+
+  # display in console
+  for (ln in l) {
+    cat(ln, "\n")
+  }
+}
+
+# get pval string ----
+pval.string <- function(pval,
+                        pstr=TRUE,
+                        stars=TRUE,
+                        sigdig=2) {
+  starstr <- ""
+  if (stars) {
+    if (pval < .001) {
+      starstr <- "***"
+    } else if (pval < .01) {
+      starstr <- "**"
+    } else if (pval < .05) {
+      starstr <- "*"
+    } else if (pval < .1) {
+      starstr <- "†"
+    }
+  }
+
+  operator <- "="
+  if (pval < .001) {
+    operator <- "<"
+    pvalstr <- ".001"
+  } else if (pval < 1) {
+    pvalstr <- toString(signif(pval, sigdig))
+    pvalstr <- substr(pval, 2, nchar(pvalstr))
+  } else if (pval == 1) {
+    pvalstr <- "1"
+  }
+
+  if (pstr) {
+    pvalstr <- paste("*p*", operator, pvalstr)
+  } else if (pval < .001) {
+    pvalstr <- paste(operator, pvalstr, sep="")
+  }
+
+  if (stars) {
+    pvalstr <- paste(pvalstr, starstr, sep="")
+  }
+
+  return(pvalstr)
+
+}
