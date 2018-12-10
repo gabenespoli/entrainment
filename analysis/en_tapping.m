e7c2ba8922ad2aef29dff7fbef386d6ed507c645
Gabe Nespoli <gabenespoli@gmail.com>
2018-12-10 07:12:05 -0500

    en_tapping: add doc comments

--- analysis/en_tapping.m
+++ analysis/en_tapping.m
@@ -1,5 +1,13 @@
 function en_tapping(ids, numMarkersPrompt)
+% Function for looping files and doing tapping preprocessing and entrainment analysis
+%
 %   numMarkersPrompt: 0 to cancel, 1 to prompt, 2 to continue, default 0
+%       This is mainly used for debugging. This is what to do if the
+%       wrong number of audio markers are found. 0 cancels and doesn't do
+%       the epoching, 2 will continue with the epoching anyway, and 1
+%       will pause execution and prompt for what to do. See the 
+%       findAudioMarkers.m function for more detail.
+
 if nargin < 1 || isempty(ids)
     d = en_load('diary', 'incl');
     ids = d.id;
